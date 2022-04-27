a35d8cb80878e4e045e32de85ae7b63e15286efc
mxbunny <mxbunnyeve@gmail.com>
2022-04-28 02:49:54 +0700

    fixed logo images not showing

--- workoutio/workoutio/src/Components/Profile/Profile.jsx
+++ workoutio/workoutio/src/Components/Profile/Profile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-
+import ProfilePic from '../../Images/profilepic001_resize.jpg';
 import './Profile.css';
 
 const Profile = () => {
-    return (
-      <div className="profile">
-        <img className="profile-pic" src=".\Images\profilepic001_resize.jpg" alt="profilepic" />
-        <p className="profile-name"> Sarah Doely </p>
-      </div>
-    );
+  return (
+    <div className="profile">
+      <img className="profile-pic" src={ProfilePic} alt="profilepic" />
+      <p className="profile-name"> Sarah Doely </p>
+    </div>
+  );
 }
 
 export default Profile;
