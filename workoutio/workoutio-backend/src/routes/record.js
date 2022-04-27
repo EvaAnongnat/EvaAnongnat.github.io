5f6621281b926eed66e8d8f91037106db73f526a
mxbunny <mxbunnyeve@gmail.com>
2022-04-28 04:43:56 +0700

    update record.js

--- workoutio/workoutio-backend/src/routes/record.js
+++ workoutio/workoutio-backend/src/routes/record.js
@@ -1,62 +1,32 @@
-const express = require('express');
+const mongoose = require("mongoose");
 
-const RecordModel = require('../models/record');
-
-const router = express.Router();
-
-router.use('/:recordId', async (req, res, next) => {
-  const recordId = req.params.recordId;
-  // Check if recordId is a valid mongodb objectId
-  if (recordId && !recordId.match(/^[0-9a-fA-F]{24}$/)) {
-    // You can response 400 too since client should request with valid it
-    // But this way it's easier to handle status code
-    return res.status(404).send('Record not found');
-  }
-  const foundRecord = await RecordModel.findById(recordId);
-  if (!foundRecord) {
-    return res.status(404).send('Record not found');
-  }
-  req.record = foundRecord;
-  // req.recordIndex = index;
-  return next();
+const recordSchema = new mongoose.Schema({
+  activity: [
+    {
+      id: {
+        type: String,
+        minlength: [5],
+        maxLength: [6],
+      },
+      name: {
+        type: String,
+        minlength: [3],        
+      },
+      src: {
+        type: String,
+        minlength: [17],
+      },
+      type: {
+        type: String,        
+      },
+    },
+  ],
+  actDate: { type: String, length:[10] },
+  quantity: { type: String, minlength: [3, "Quantity should contains at least 3 char"] },
+  duration: { type: String, } ,
+  timestamp: { type: Date }
 });
 
-router.get('/:recordId', (req, res, next) => {
-  return res.send(req.record);
-});
+const RecordModel = mongoose.model("Record", recordSchema, "records");
 
-router.get('/', async (req, res, next) => {
-  const records = await RecordModel.find({});
-  res.send(records);
-});
-
-router.post('/', async (req, res, next) => {
-  const body = req.body;
-
-  const newRecord = new RecordModel(body);
-
-  const errors = newRecord.validateSync();
-  if (errors) {
-    const errorFieldNames = Object.keys(errors.errors);
-    if (errorFieldNames.length > 0) {
-      return res.status(400).send(errors.errors[errorFieldNames[0]].message);
-    }
-  }
-
-  await newRecord.save();
-
-  return res.status(201).send(newRecord);
-});
-
-router.put('/:recordId', async (req, res, next) => {
-  const body = req.body;
-  const updateRecords = await RecordModel.updateOne(req.RecordModel, body);
-  return res.send(updateRecords)
-});
-
-router.delete('/:recordId', async (req, res, next) => {
-  const deleteRecord = await RecordModel.deleteOne(req.record);
-  return res.status(204).send(deleteRecord); // 204 = No content which mean it successfully removed
-});
-
-module.exports = router;
+module.exports = RecordModel;
