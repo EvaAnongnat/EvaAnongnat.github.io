2d509b0380f13e44fb54e554e0ff89301c87019f
mxbunny <mxbunnyeve@gmail.com>
2022-04-28 13:30:57 +0700

    deploy to vercel

--- workoutio/workoutio-backend/src/app.js
+++ workoutio/workoutio-backend/src/app.js
@@ -1,6 +1,38 @@
-const mongoose = require("mongoose");
-const app = require("../api/index");
-const config = require("./config");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 4001;
+const config = require('./config');
+const userRouter = require('./routes/user');
+
+const app = express();
+
+if (config.isVercel) {
+  app.use(async (req, res, next) => {
+    await mongoose.connect(config.mongoUri, config.mongoOptions);
+    return next();
+  });
+}
+
+// Body parser to parse json in request body for us
+app.use(bodyParser.json());
+// CORS
+app.use(
+  cors({
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+
+app.use('/users', userRouter);
+
+
+app.get('/', (req, res) =>  {
+  res.send("Server is running");
+})
+
+
 
 
 const boot = async () => {
@@ -13,3 +45,4 @@ const boot = async () => {
 };
 
 boot();
+module.exports = app;
