83695ad9d94dc378fc0aeadddb84a0b97cd8e002
mxbunny <mxbunnyeve@gmail.com>
2022-04-28 12:38:55 +0700

    repdeploy to vercel

--- workoutio/workoutio-backend/api/index.js
+++ workoutio/workoutio-backend/api/index.js
@@ -1,35 +1,41 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dotenv').config();
-const config = require("../src/config");
+const PORT = process.env.PORT || 4001;
+const config = require('../src/config');
+
+const userRouter = require('../src/routes/user');
 
 const app = express();
-const PORT = config.port;
 
-// const recordRouter = require("./routes/records");
-const route = require("../src/routes/user");
-
-
-// if (config.isVercel) {
 if (config.isVercel) {
   app.use(async (req, res, next) => {
     await mongoose.connect(config.mongoUri, config.mongoOptions);
     return next();
   });
 }
+
+// Body parser to parse json in request body for us
 app.use(bodyParser.json());
+// CORS
 app.use(
   cors({
-    origin: "*",
-    optionsSuccessStatus: 200,
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
 
-app.use("/users", route);
+// Our routers
+app.use('/users', userRouter);
+
+
 app.get('/', (req, res) =>  {
-    res.send("Server running");
-  })
+  res.send("Server is running");
+})
+
+
+
+
 module.exports = app;
